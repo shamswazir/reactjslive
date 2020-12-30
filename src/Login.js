dab3824f325f077e89b104f336e0efc783846576
shamswazir <shamswazir780@gmail.com>
2020-12-30 18:01:02 +0530

    setup url

--- src/Login.js
+++ src/Login.js
@@ -73,4 +73,4 @@ function Login() {
     )
 }
 
-export default Login
+export default Login;
--- src/Registration.js
+++ src/Registration.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {Login} from './Login';
+import Login from './Login';
 
 function Registration() {
     
